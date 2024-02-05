32dbf26bbc141736bdcf3425ab86e7da6bea6fd1
Emylli Soares <100449171+emyllisoares@users.noreply.github.com>
2024-02-05 17:02:24 +0000

    aaaa

--- proj/functions.c
+++ proj/functions.c
@@ -72,7 +72,7 @@ void adicionarContato(Contato contatos[MAX_CONTATO], int *numcontatos){
         scanf("%[^\n]", contatos[*numcontatos].nome);
         limparBuffer();
 
-        printf(WHITE"Telefone (formato +55 xx xxxx-xxxx): "FIM_COR);
+        printf(WHITE"Telefone (formato +55 xx xxxxx-xxxx): "FIM_COR);
         scanf("%[^\n]", contatos[*numcontatos].telefone);
         limparBuffer();
 
@@ -101,7 +101,7 @@ void adicionarContato(Contato contatos[MAX_CONTATO], int *numcontatos){
             limparTerminal();
             printf(WHITE"Nome: "FIM_COR);
             printf("%s\n", contatos[*numcontatos].nome);
-            printf(WHITE"Telefone (formato +55 xx xxxx-xxxx): "FIM_COR);
+            printf(WHITE"Telefone (formato +55 xx xxxxx-xxxx): "FIM_COR);
             printf("%s\n", contatos[*numcontatos].telefone);
             printf(WHITE"Email: "FIM_COR);
             scanf("%s", contatos[*numcontatos].email);
@@ -111,7 +111,7 @@ void adicionarContato(Contato contatos[MAX_CONTATO], int *numcontatos){
         limparTerminal();
         printf(WHITE"Nome: "FIM_COR);
         printf("%s\n", contatos[*numcontatos].nome);
-        printf(WHITE"Telefone (formato +55 xx xxxx-xxxx): "FIM_COR);
+        printf(WHITE"Telefone (formato +55 xx xxxxx-xxxx): "FIM_COR);
         printf("%s\n", contatos[*numcontatos].telefone);
         printf(WHITE"Email: "FIM_COR);
         printf("%s\n", contatos[*numcontatos].email);
